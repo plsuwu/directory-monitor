b80f5f1b2c2e7ef976ac3b3b1e829b77a9512091
plsuwu <124419933+plsuwu@users.noreply.github.com>
2026-09-01 07:23:57 +1000

    "complete" watcher implementation

--- src/watcher.c
+++ src/watcher.c
@@ -13,24 +13,36 @@
 
 #define WATCH_DIR "./example-dir"
 
+/// Stub for interfacing with a work queue. In practice, this would do event
+/// handoff to the indexer, which makes `statx` calls to diff against existing
+/// state
+typedef enum {
+    /// File content changed hint
+    SYNC_CONTENT_CHANGED,
+
+    // Manifest deletion hint
+    SYNC_ENTRY_REMOVED,
+
+    // Recreate at new path hint
+    SYNC_ENTRY_MOVED_IN,
+
+    // Reconciliation scan hint
+    SYNC_FULL_RESCAN
+} sync_action;
+
+typedef struct {
+    char *buf;
+    size_t len;
+} watch_dir;
+
 typedef struct {
     int32_t fan_fd;
     int32_t mnt_fd;
+    watch_dir dir;
 } watcher;
 
-/// Stub for interfacing with a work queue.
-///
-/// In practice, this would do event handoff to the indexer, which makes `statx`
-/// calls to diff against existing state.
-typedef enum {
-    SYNC_CONTENT_CHANGED, // emit hint to indexer
-    SYNC_ENTRY_REMOVED, // manifest deletion
-    SYNC_ENTRY_MOVED_IN, // create/modify at new path
-    SYNC_FULL_RESCAN // kernel queue overflow and should reconcile actual state
-} sync_action;
-
 /// Mocks event enqueuer functionality: as per above, we might do  handoff to
-/// the indexer via an MPSC message or something like this.
+/// the indexer via an MPSC message (or something like this)
 static void enqueue(sync_action action, const char *path) {
     static const char *names[] =
         {"CONTENT_CHANGED", "ENTRY_REMOVED", "MOVED_IN", "FULL_RESCAN"};
@@ -38,6 +50,7 @@ static void enqueue(sync_action action, const char *path) {
     printf("[queue] %-15s %s\n", names[action], path ? path : "(all)");
 }
 
+/// Runs `watcher` file descriptor cleanup
 static void watcher_clean(watcher *w) {
     printf("running cleanup\n");
     if (w->mnt_fd) {
@@ -49,39 +62,44 @@ static void watcher_clean(watcher *w) {
     }
 }
 
+/// Initializes a file descriptors in a `watcher` struct and adds a mark to the
+/// fanotify instance. Initialization is performed by populating the relevant
+/// file descriptors `fan_fd` & `mnt_fd` via the relevant syscalls.
 static int watcher_init(watcher *w, const char *mnt_path) {
     int result;
 
-    // `FAN_REPORT_DFID_NAME` is "shorthand" for
-    // `FAN_REPORT_DIR_FID | FAN_REPORT_NAME`
-    w->fan_fd = fanotify_init(
-        FAN_CLASS_NOTIF | FAN_REPORT_DFID_NAME | FAN_NONBLOCK,
-        O_CLOEXEC | O_RDONLY
-    );
-
-    if (w->fan_fd == -1) {
+    // open fanotify instance
+    if ((w->fan_fd = fanotify_init(
+             FAN_CLASS_NOTIF | FAN_REPORT_DFID_NAME | FAN_NONBLOCK,
+             O_CLOEXEC | O_RDONLY
+         ))
+        == -1)
+    {
         perror("fanotify_init");
+
         result = -1;
         goto _ep;
     }
 
-    w->mnt_fd = open(mnt_path, O_DIRECTORY | O_CLOEXEC);
-    if (w->mnt_fd == -1) {
+    // open filesystem for reading
+    if ((w->mnt_fd = open(mnt_path, O_DIRECTORY | O_CLOEXEC)) == -1) {
         perror("open");
 
         result = -1;
         goto _ep;
     }
 
+    // add a filesystem-wide mark
     if (fanotify_mark(
             w->fan_fd,
             FAN_MARK_ADD | FAN_MARK_FILESYSTEM,
             FAN_CLOSE_WRITE | FAN_DELETE | FAN_MOVED_FROM | FAN_MOVED_TO
                 | FAN_ONDIR,
             AT_FDCWD,
-            WATCH_DIR
+            mnt_path
         )
-        < 0) {
+        < 0)
+    {
         perror("fanotify_mark");
 
         result = -1;
@@ -89,23 +107,18 @@ static int watcher_init(watcher *w, const char *mnt_path) {
     }
 
     result = 0;
+
 _ep:
-    // handle any cleanup on failure
     if (result == -1) {
         watcher_clean(w);
-    } else {
-        printf("opened: fan_fd=%d, mnt_fd=%d\n", w->fan_fd, w->mnt_fd);
-        fprintf(
-            stderr,
-            "mount_fd=%d valid=%d\n",
-            w->mnt_fd,
-            fcntl(w->mnt_fd, F_GETFD) >= 0
-        );
     }
 
     return result;
 }
 
+/// Resolves a file handle to a directory into its path. ESTALE is occasionally
+/// expected, and so shouldn't be fatal - the entry was deleted/renamed before
+/// we could resolve it, which is intended to be reconciled via periodic rescans
 static int watcher_resolve_path(
     const watcher *w,
     const struct file_handle *fh,
@@ -114,21 +127,22 @@ static int watcher_resolve_path(
     size_t out_len
 ) {
     char proc[64];
+    int fd;
+    ssize_t n;
+    size_t used;
 
-    int fd = open_by_handle_at(
+    fd = open_by_handle_at(
         w->mnt_fd,
         (struct file_handle *)fh,
         O_PATH | O_CLOEXEC
     );
-
-    printf("fd -> %d\n", fd);
 
     if (fd < 0) {
         return -1;
     }
 
     snprintf(proc, sizeof proc, "/proc/self/fd/%d", fd);
-    ssize_t n = readlink(proc, out, out_len - 1);
+    n = readlink(proc, out, out_len - 1);
 
     close(fd);
     if (n < 0) {
@@ -136,9 +150,8 @@ static int watcher_resolve_path(
     }
 
     out[n] = '\0';
-
     if (name && name[0] && strcmp(name, ".") != 0) {
-        size_t used = (size_t)n;
+        used = (size_t)n;
         if (used + 1 + strlen(name) + 1 > out_len) {
             errno = ENAMETOOLONG;
             return -1;
@@ -151,6 +164,18 @@ static int watcher_resolve_path(
     return 0;
 }
 
+/// Check whether an `event` filepath starts with the `char *` `target`
+static int
+filter_by_prefix(const char *target, const char *event, const size_t length) {
+    if (strlen(event) < length || (memcmp(target, event, length)) != 0) {
+        return 0;
+    }
+
+    return 1;
+}
+
+/// Handle a `fanotify` event, using the event metadata to retrieve DFID_NAME
+/// records for a given event.
 static void
 handle_event(const watcher *w, const struct fanotify_event_metadata *md) {
     const struct fanotify_event_info_fid *fid = NULL;
@@ -159,18 +184,21 @@ handle_event(const watcher *w, const struct fanotify_event_metadata *md) {
     char *name;
     char path[PATH_MAX + 1];
 
-    // kernel queue overflow, which is generally indicative of having lost
-    // events; we would want to do a full rescan to reconcile lost dir state
+    // kernel queue overflow - we have missed events and should reconcile state
+    // via a full rescan
     if (md->mask & FAN_Q_OVERFLOW) {
         enqueue(SYNC_FULL_RESCAN, NULL);
         return;
     }
 
-    for (ih = (const void *)(md + 1);
-         (const char *)ih < (const char *)md + md->event_len;
-         ih = (const void *)((const char *)ih + ih->len)) {
+    for (const char *p = (((const char *)md) + md->metadata_len);
+         p + sizeof *ih <= (((const char *)md) + md->event_len);
+         p += ih->len)
+    {
+        // update current `ih->len` for next iteration
+        ih = (const struct fanotify_event_info_header *)p;
         if (ih->info_type == FAN_EVENT_INFO_TYPE_DFID_NAME) {
-            fid = (struct fanotify_event_info_fid *)ih;
+            fid = (struct fanotify_event_info_fid *)p;
             break;
         }
     }
@@ -181,13 +209,17 @@ handle_event(const watcher *w, const struct fanotify_event_metadata *md) {
 
     fh = (const struct file_handle *)fid->handle;
     name = (char *)fh->f_handle + fh->handle_bytes;
-
-    printf("fh=%p,name=%s\n", fh, name);
-
     if (watcher_resolve_path(w, fh, name, path, sizeof(path)) < 0) {
         if (errno != ESTALE) {
             perror("watcher_resolve_path");
         }
+        return;
+    }
+
+    // we might do something like cache this by its file handle so we avoid
+    // calling (the comparatively expensive) `watcher_resolve_path` above, but
+    // this Also Works
+    if (!(filter_by_prefix(w->dir.buf, path, w->dir.len))) {
         return;
     }
 
@@ -202,19 +234,19 @@ handle_event(const watcher *w, const struct fanotify_event_metadata *md) {
     }
 }
 
+/// Drain everything currently readable on the `fanotify` instance. Called by
+/// `epoll` when it reports that the fd is readable
 static void watcher_drain(const watcher *w) {
     ssize_t len;
     char buf[8192] __attribute__((aligned(8)));
     const struct fanotify_event_metadata *md;
 
-    for (;;) {
+    while (1) {
         len = read(w->fan_fd, buf, sizeof buf);
         if (len < 0) {
             if (errno == EAGAIN) {
-                // fully drained
-                return;
+                return; // fully drained
             }
-
             if (errno == EINTR) {
                 continue;
             }
@@ -223,21 +255,14 @@ static void watcher_drain(const watcher *w) {
             return;
         }
 
-        // --------------------------------------------------------------------
-        // TODO not a huge fan of having the side effect below in this function
-        // and i would prefer to return the buf (or metadata or whatever) and
-        // read through events in the caller rather than here.
-        // --------------------------------------------------------------------
-
         md = (const void *)buf;
-        printf("got event: len=%lu\n", len);
 
         while (FAN_EVENT_OK(md, len)) {
             if (md->vers != FANOTIFY_METADATA_VERSION) {
                 fprintf(stderr, "fanotify ABI mismatch\n");
                 exit(EXIT_FAILURE);
             }
-
+            // TODO: not a huge fan of this side effect but it stays for now...
             handle_event(w, md);
             md = FAN_EVENT_NEXT(md, len);
         }
@@ -245,15 +270,25 @@ static void watcher_drain(const watcher *w) {
 }
 
 int main(void) {
-    struct epoll_event ev;
+    watch_dir watchdir;
     int ep;
+    struct epoll_event ev;
+
+    // we just use the hardcoded directory at the top of the file
+    if ((watchdir.buf = realpath(WATCH_DIR, NULL)) == NULL) {
+        perror("realpath");
+        return EXIT_FAILURE;
+    }
+
+    watchdir.len = strlen(watchdir.buf);
 
     watcher w = {
+        .dir = watchdir,
         .fan_fd = -1,
         .mnt_fd = -1,
     };
 
-    if ((watcher_init(&w, WATCH_DIR)) != 0) {
+    if ((watcher_init(&w, watchdir.buf)) != 0) {
         printf("`fanotify` instance init failure\n");
         return EXIT_FAILURE;
     }
@@ -268,8 +303,8 @@ int main(void) {
         return EXIT_FAILURE;
     }
 
-    printf("watching: %s\n", WATCH_DIR);
-    for (;;) {
+    printf("watching: %s\n", watchdir.buf);
+    while (1) {
         struct epoll_event out[4];
         int n = epoll_wait(ep, out, 4, -1);
         if (n < 0) {
